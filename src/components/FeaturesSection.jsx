58a35a9ee5be0164309e8d97acc645bad224566c
N M Nayana <nmnayana62@gmail.com>
2024-06-19 11:25:20 +0530

    added fancybox

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -2,16 +2,11 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import ScrollImages from "./Scroll";
 import CircularImages from "./CircularImages";
+import LightBoxImages from "./LightBoxImages";
 
 export default function FeaturesSection() {
   const [showImages, setShowImages] = useState("exteriors");
-
-  const exteriors = ["/assets/img1.svg", "/assets/img6.jpeg"];
-  const interiors = ["/assets/img2.svg", "/assets/img5.svg"];
-
-  const images = showImages === "exteriors" ? exteriors : interiors;
 
   return (
     <div className="max-w-[1250px] m-auto h-fit flex flex-col items-center py-16 px-5 xl:px-0">
@@ -60,47 +55,7 @@ export default function FeaturesSection() {
           Interiors
         </button>
       </div>
-      <div className="w-full h-[222px] md:h-[494px] my-3 md:my-5">
-        <ScrollImages images={images} />
-      </div>
-      <div className="w-full flex gap-1 lg:gap-0 justify-normal lg:justify-between">
-        <div>
-          <Image
-            loading="lazy"
-            width={265}
-            height={168}
-            src={"/assets/img2.svg"}
-            alt="img"
-          />
-        </div>
-        <div>
-          <Image
-            loading="lazy"
-            width={265}
-            height={168}
-            src={"/assets/img3.svg"}
-            alt="img"
-          />
-        </div>
-        <div>
-          <Image
-            loading="lazy"
-            width={265}
-            height={168}
-            src={"/assets/img4.svg"}
-            alt="img"
-          />
-        </div>
-        <div>
-          <Image
-            loading="lazy"
-            width={265}
-            height={168}
-            src={"/assets/img5.svg"}
-            alt="img"
-          />
-        </div>
-      </div>
+      <LightBoxImages />
     </div>
   );
 }
---
+++ src/components/LightBoxImages.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect } from "react";
+import Image from "next/image";
+import { Fancybox as NativeFancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+const LightBoxImages = () => {
+  useEffect(() => {
+    NativeFancybox.bind("[data-fancybox]", {
+      AnimationDuration: 300,
+    });
+
+    return () => {
+      NativeFancybox.destroy();
+    };
+  }, []);
+
+  const images = [
+    { src: "/assets/img1.svg", alt: "img1" },
+    { src: "/assets/img2.svg", alt: "img2" },
+    { src: "/assets/img3.svg", alt: "img3" },
+    { src: "/assets/img4.svg", alt: "img4" },
+    { src: "/assets/img5.svg", alt: "img5" },
+  ];
+
+  return (
+    <div className="w-full">
+      <div className="w-full h-[222px] md:h-[494px] relative my-3 md:my-5">
+        <div className="absolute inset-0">
+          <a data-fancybox="gallery" href={images[0].src}>
+            <Image
+              layout="fill"
+              objectFit="cover"
+              src={images[0].src}
+              alt={images[0].alt}
+              className="rounded-[14px]"
+            />
+          </a>
+        </div>
+        <div>
+          <div className="absolute w-[34px] h-[34px] top-1/2 left-4 pointer-events-none transform -translate-y-1/2 bg-customWhite text-xl rounded-full shadow-md flex justify-center items-center">
+            <Image
+              width={10}
+              height={10}
+              src={"/assets/left_arrow.svg"}
+              alt="left-arrow"
+            />
+          </div>
+          <div className="absolute w-[34px] h-[34px] top-1/2 right-4 pointer-events-none transform -translate-y-1/2 bg-customWhite rounded-full shadow-md flex justify-center items-center">
+            <Image
+              width={10}
+              height={10}
+              src={"/assets/right_arrow.svg"}
+              alt="right-arrow"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex gap-1 lg:gap-0 justify-normal lg:justify-between">
+        {images.slice(1).map((image) => (
+          <div key={image}>
+            <a data-fancybox="gallery" href={image.src}>
+              <Image
+                loading="lazy"
+                width={300}
+                height={200}
+                src={image.src}
+                alt={image.alt}
+              />
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LightBoxImages;
